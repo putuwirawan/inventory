4a0152f07689bf46acf494ca1d8e9a7937f65060
putuwirawan <putuwirawan79@gmail.com>
2020-05-28 10:31:11 +0800

    28-05-2020-1

--- android/app/src/main/java/com/wawanlahne/MainActivity.java
+++ android/app/src/main/java/com/wawanlahne/MainActivity.java
@@ -1,24 +1,28 @@
 package com.wawanlahne;
 
 import com.facebook.react.ReactActivity;
+// import com.reactnativecommunity.picker.RNCPickerPackage;
 // import com.BV.LinearGradient.LinearGradientPackage;
 
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule rendering of the component.
    */
   @Override
   protected String getMainComponentName() {
     return "WawanLahne";
   }
-
-//   @Override
-// protected List<ReactPackage> getPackages() {
-//   return Arrays.<ReactPackage>asList(
-//     new MainReactPackage(),
-//     new LinearGradientPackage() // <---- and This! 
-//   );
-// }
+  // @Override
+  // protected getPackages(){
+  //   new RNCPickerPackage();
+  // }
+  // @Override
+  // protected List<ReactPackage> getPackages() {
+  // return Arrays.<ReactPackage>asList(
+  // new MainReactPackage(),
+  // new LinearGradientPackage() // <---- and This!
+  // );
+  // }
 }
--- src/pages/MenuPage/Shipping/ShippingPage.js
+++ src/pages/MenuPage/Shipping/ShippingPage.js
@@ -3,7 +3,8 @@ import React, {useState, useEffect} from 'react';
 import {
   View,
   Text,
-  Picker,
+  ScrollView,
+  SectionList,
   StyleSheet,
   Platform,
   ActivityIndicator,
@@ -14,13 +15,18 @@ import AsyncStorage from '@react-native-community/async-storage';
 import {TextInput} from 'react-native-paper';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import {Dropdown} from 'react-native-material-dropdown';
+import {Picker} from '@react-native-community/picker';
+import {FlatList} from 'react-native-gesture-handler';
 
 const ShippingPage = () => {
+  const [isLoadingShipping, setIsLoadingShipping] = useState(true);
   const {colors} = useTheme();
-  const [shippngProces, setShippingProces] = useState(1);
-
-  const data = [];
-  const [storeState, setStoreState] = useState(data[0]);
+  const [shippingProces, setShippingProces] = useState(1);
+  const [storeDatas, setStoreDatas] = useState([]);
+  const [shippingDatas, setShippingDatas] = useState([]);
+  const [totalCount, setTotalCount] = useState(0);
+  const [selectedStore, setSelectedStore] = useState('');
+  const [textSearch, setTextSearch] = useState('');
 
   const getStore = async () => {
     const token = await AsyncStorage.getItem('userToken');
@@ -37,16 +43,89 @@ const ShippingPage = () => {
       },
     )
       .then((response) => response.json())
-      .then((datas) => {
-        datas.map(
-          async (item) => await data.push({value: item.Id, label: item.Code}),
-        );
+      .then(async (datas) => {
+        let data = [];
+        if (datas.length) {
+          await datas.map(
+            async (item) => await data.push({value: item.Id, label: item.Code}),
+          );
+        }
+        await setStoreDatas(data);
+      })
+      .catch((e) => {
+        console.log(e);
       });
   };
-  useEffect(() => {
-    getStore();
-  });
+  const getShipping = async () => {
+    const token = await AsyncStorage.getItem('userToken');
+    setIsLoadingShipping(true);
 
+    await fetch(
+      `http://inventoryapi.planetsurf.id/api/v1/Shippings/GetShippingDocs?store=${selectedStore}&status=${shippingProces}`,
+
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + token,
+        },
+        body: JSON.stringify({
+          search: textSearch,
+          take: '10',
+          skip: '0',
+          sort: 'Courier',
+          order: 'true',
+        }),
+      },
+    )
+      .then((response) => response.json())
+      .then(async (datas) => {
+        setTotalCount(datas.totalCount);
+        let data = [];
+        if (datas.data.length) {
+          await datas.data.map((item) => {
+            data.push(item);
+          });
+        }
+        setShippingDatas(data);
+        setIsLoadingShipping(false);
+      })
+      .catch((e) => {
+        console.log(e.toString());
+      });
+  };
+
+  const RenderItem = ({item}) => {
+    return (
+      <View style={[styles.action, {backgroundColor: colors.background}]}>
+        <View>
+          <Text style={[styles.textHeader, {color: colors.text}]}>
+            Shipping: {item.ShippingNumber}
+          </Text>
+          <Text style={{color: colors.text}}>Sender: {item.Sender}</Text>
+          <Text style={{color: colors.text}}>Courir: {item.Courier}</Text>
+        </View>
+        <View>
+          <Text style={[styles.textHeader, {color: colors.text}]}>
+            Store: {item.Store}
+          </Text>
+          <Text style={{color: colors.text}}>TotalQty: {item.TotalQty}</Text>
+          <Text style={{color: colors.text}}>ShipmentMethod: {item.StrShipmentMethod}</Text>
+        </View>
+      </View>
+    );
+  };
+  async function initData() {
+    await getStore();
+    await getShipping();
+  }
+  useEffect(() => {
+    initData();
+  }, []);
+  useEffect(() => {
+    getShipping();
+  }, [shippingProces, selectedStore, textSearch]);
   return (
     <View style={styles.container}>
       <View
@@ -68,7 +147,9 @@ const ShippingPage = () => {
           <TextInput
             placeholder="Search"
             style={[styles.textSearch, {color: colors.text, width: 250}]}
-            onChangeText={() => {}}
+            onChangeText={(val) => {
+              setTextSearch(val);
+            }}
           />
         </View>
       </View>
@@ -76,36 +157,53 @@ const ShippingPage = () => {
       <View style={styles.section}>
         <Picker
           mode="dropdown"
-          selectedValue={shippngProces}
+          selectedValue={shippingProces}
           style={{width: 150, color: colors.text}}
-          onValueChange={(itemValue) => setShippingProces(itemValue.value)}>
+          onValueChange={async (itemSelect, itemIndex) => {
+            await setShippingProces(itemSelect);
+          }}>
           <Picker.Item label=" On Delivery" value={1} />
           <Picker.Item label="Delivered" value={2} />
         </Picker>
-        {storeState === null ? (
-          <View style={{justifyContent: 'center', alignItems: 'center'}}>
-            <ActivityIndicator size="large" />
-          </View>
-        ) : (
-          <Dropdown
-            value={storeState}
-            data={data}
-            fontSize={18}
-            dropdownOffset={{top: 0, left: 0}}
-            baseColor="#66E7A7"
-            textColor={colors.text}
-            selectedItemColor="#33654C"
-            containerStyle={{width: '40%'}}
-            onChangeText={(selectValue) => {
-              console.log(selectValue);
-              setStoreState(selectValue);
-            }}
-          />
-        )}
+        <Dropdown
+          value="Select Store"
+          animationDuration={500}
+          data={storeDatas}
+          fontSize={18}
+          dropdownOffset={{top: 0, left: 0}}
+          dropdownPosition={-2}
+          baseColor={colors.text}
+          textColor={colors.text}
+          selectedItemColor="#33654C"
+          containerStyle={{width: '40%'}}
+          onChangeText={(selectValue) => {
+            setSelectedStore(selectValue);
+          }}
+        />
       </View>
-      <View style={{paddingVertical: 10, paddingHorizontal: 10}}>
-        <Text style={{color: 'red'}}>Kopi ne ru </Text>
-      </View>
+      <ScrollView>
+        <View
+          style={{paddingVertical: 10, paddingHorizontal: 10}}
+          backgroundColor="#9FDABC">
+          <Text>{shippingProces}</Text>
+          <Text>total Count {totalCount}</Text>
+          {isLoadingShipping ? (
+            <ActivityIndicator
+              size="large"
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}></ActivityIndicator>
+          ) : (
+            <FlatList
+              data={shippingDatas}
+              renderItem={RenderItem}
+              keyExtractor={(item) => item.id}
+            />
+          )}
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -116,7 +214,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  section: {flexDirection: 'row', alignItems: 'center', marginRight: 20},
+  section: {flexDirection: 'row', alignItems: 'center', marginRight: 20, justifyContent: 'space-between'},
   textSearch: {
     flex: 1,
     marginTop: Platform.OS === 'ios' ? 0 : 2,
@@ -124,12 +222,19 @@ const styles = StyleSheet.create({
     color: '#05375a',
     height: 25,
   },
+  textHeader: {
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
   action: {
+    justifyContent: 'space-between',
     flexDirection: 'row',
     marginTop: 10,
     borderBottomWidth: 1,
     borderBottomColor: '#f2f2f2',
-    paddingBottom: 5,
+    padding: 10,
+
+    borderRadius: 10,
   },
   dropdown: {
     width: '40%',
